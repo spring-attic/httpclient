5264970f3f08aa53f8582ae4ca2bfc14bba84e86
Soby Chacko <schacko@pivotal.io>
2016-11-04 11:27:48 -0400

    test debugging

---
+++ spring-cloud-starter-stream-processor-httpclient/src/test/java/org/springframework/cloud/stream/app/httpclient/processor/HttpClientTests.java
@@ -0,0 +1,14 @@
+package org.springframework.cloud.stream.app.httpclient.processor;
+
+import org.junit.Test;
+
+/**
+ * @author Soby Chacko
+ */
+public class HttpClientTests {
+
+	@Test
+	public void test() {
+		//TODO: please remove this test when other tests can be run through maven
+	}
+}
